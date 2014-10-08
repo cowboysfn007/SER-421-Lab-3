a7c7b24aa2325b17d9e2cc5f8c4330e04c5ae444
Russ Parmer <rparmer02@gmail.com>
2014-10-08 15:44:53 -0700

    created initial object requests

--- resources/js/skynet.js
+++ resources/js/skynet.js
@@ -1,9 +1,23 @@
 function createDevice(){
   console.log("It Worked");
+  
+  var request = getRequestObject();
+  request.onreadystatechange = function() {handleRequest(request)};
+  request.open("GET", "http://skynet.im/status", true);
+  request.send(null);
 }
 
 function addField(divName){
     var newDiv = document.createElement('div');
-    newDiv.innerHTML = "Key/Value Pair: <input type='text' name='keyInputs[]'>-<input type='text' name='valueInputs[]'>"
+    newDiv.innerHTML = "Key/Value Pair: <input type='text' name='keyInputs[]'>=<input type='text' name='valueInputs[]'>"
     document.getElementById(divName).appendChild(newDiv);
+}
+  
+function getRequestObject() {
+  if (window.XMLHttpRequest) return (new XMLHttpRequest());
+  else return (null);
+}
+
+function handleRequest(request) {
+  if (request.readyState == 4) console.log(request.responseText);
 }
