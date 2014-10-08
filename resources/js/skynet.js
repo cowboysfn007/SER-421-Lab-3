49c2c34335f2b47acf31319f45d1255fc3a6a7e5
Rob Miller <rpmille6@asu.edu>
2014-10-07 17:21:46 -0700

    added functionality for dynamic key value pairs

--- resources/js/skynet.js
+++ resources/js/skynet.js
@@ -2,6 +2,8 @@ function createDevice(){
   console.log("It Worked");
 }
 
-function addField(){
-    
+function addField(divName){
+    var newDiv = document.createElement('div');
+    newDiv.innerHTML = "<br><input type='text' name='keyInputs[]'>-<input type='text' name='valueInputs[]'>"
+    document.getElementById(divName).appendChild(newDiv);
 }
