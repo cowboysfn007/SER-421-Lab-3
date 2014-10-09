d119215e2dc0c10baa708fa7a13348683a221255
Russ Parmer <rparmer02@gmail.com>
2014-10-08 20:32:27 -0700

    implemented createDevice functionality

--- resources/js/skynet.js
+++ resources/js/skynet.js
@@ -1,10 +1,22 @@
 function createDevice(){
   console.log("It Worked");
   
+  var keys = document.getElementsByName("keyInputs[]");
+  var values = document.getElementsByName("valueInputs[]");
+  var payload = "";
+  
+  for (var i=0; i < keys.length; i++) {
+    if ((keys[i].value.length > 0) && (values[i].value.length > 0)) {
+      if (payload.length > 0) payload = payload + "&";
+      payload = payload + keys[i].value + "=" + values[i].value;
+    }
+  }
+  
   var request = getRequestObject();
   request.onreadystatechange = function() {handleRequest(request)};
-  request.open("GET", "http://skynet.im/status", true);
-  request.send(null);
+  request.open("POST", "http://skynet.im/devices", true);
+  request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  request.send(payload);
 }
 
 function addField(divName){
