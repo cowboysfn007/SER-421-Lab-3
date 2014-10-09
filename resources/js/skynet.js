3e3d567993657a6d1d34862971f96490c0a17f1d
Russ Parmer <rparmer02@gmail.com>
2014-10-08 20:47:18 -0700

    forgot to read owner and token values, chanaged html from from name to id

--- resources/js/skynet.js
+++ resources/js/skynet.js
@@ -1,22 +1,27 @@
 function createDevice(){
   console.log("It Worked");
   
+  var owner = document.getElementById("owner").value;
+  var token = document.getElementById("token").value;
   var keys = document.getElementsByName("keyInputs[]");
   var values = document.getElementsByName("valueInputs[]");
-  var payload = "";
   
-  for (var i=0; i < keys.length; i++) {
-    if ((keys[i].value.length > 0) && (values[i].value.length > 0)) {
-      if (payload.length > 0) payload = payload + "&";
-      payload = payload + keys[i].value + "=" + values[i].value;
+  if ((owner.length > 0) && (token.length > 0)) {
+    var payload = "owner=" + owner + "&token=" + token;
+
+    for (var i=0; i < keys.length; i++) {
+      if ((keys[i].value.length > 0) && (values[i].value.length > 0)) {
+        payload = payload + "&"+ keys[i].value + "=" + values[i].value;
+      }
     }
-  }
   
-  var request = getRequestObject();
-  request.onreadystatechange = function() {handleRequest(request)};
-  request.open("POST", "http://skynet.im/devices", true);
-  request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-  request.send(payload);
+    var request = getRequestObject();
+    request.onreadystatechange = function() {handleRequest(request)};
+    request.open("POST", "http://skynet.im/devices", true);
+    request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    request.send(payload);
+    
+  }else console.log("invalid input");
 }
 
 function addField(divName){
